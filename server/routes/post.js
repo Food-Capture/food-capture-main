8cd4fc69b91344628b0829490e13696f58114e0c
tanjoshua <jtanjoshua@gmail.com>
2021-03-28 13:56:06 -0400

    insert auth middleware for post-related endpoints - user must attach token to access endpoints

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,18 +3,17 @@ const express = require("express");
 
 // internal imports
 const postController = require("../controllers/post");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-//TODO: insert auth middleware (do not do for now for easier testing)
-
 // POST /post
-router.post("/", postController.createPost);
+router.post("/", auth, postController.createPost);
 
 // GET /post
-router.get("/", postController.getPosts);
+router.get("/", auth, postController.getPosts);
 
 // GET /post/<postId>
-router.get("/:postId", postController.getPostDetails);
+router.get("/:postId", auth, postController.getPostDetails);
 
 module.exports = router;
